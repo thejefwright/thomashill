8493ab505783944f7f223535515c46601419bdca
Jef Wright <thejefwright@Jefs-MacBook-Pro-2.local>
2021-06-01 13:49:32 -0400

    adjusting padding on Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import Hamburger from './Hamburger';
 import Nav from './Nav';
 
 const HeaderStyles = styled.div`
-  padding: 1rem;
+  padding: 0 1rem;
   display: flex;
   align-items: center;
   justify-content: space-between;
