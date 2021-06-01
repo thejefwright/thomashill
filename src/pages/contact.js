86afffef3290ad7bd9c861b4399dbb593b082445
Jef Wright <thejefwright@Jefs-MacBook-Pro-2.local>
2021-06-01 17:40:12 -0400

    updating netlify form settings

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -58,7 +58,7 @@ export default function ContactPage() {
   return (
     <ContactStyles>
       <h1>Fill out the form to contact me.</h1>
-      <FormStyles method="POST" data-netlify="true" name="contact">
+      <FormStyles name="contact" method="POST" data-netlify="true">
         <label htmlFor="name">Name: </label>
         <input type="text" name="name" id="name" />
         <label htmlFor="email">Email: </label>
